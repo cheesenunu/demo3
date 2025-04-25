8caffc2faf043c17275e5e980d87f8621b6aa0a1
cheesenunu <1287713647@qq.com>
2025-04-25 15:45:49 +0800

    Initial commit

---
+++ src/main/java/com/yamlcommand/demo3/yamlcommand.java
@@ -0,0 +1,12 @@
+package com.yamlcommand.demo3;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class yamlcommand extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
